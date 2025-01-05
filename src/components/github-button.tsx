9fd554edd549ff46ac915cb20feb2227b60a1303
suzuki3jp <suzuki3.info@gmail.com>
2025-01-05 18:21:26 +0900

    fix: Fix a error `Missing Suspense boundary with useSearchParams`
    - https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout

--- src/components/github-button.tsx
+++ src/components/github-button.tsx
@@ -4,19 +4,22 @@ import { Button } from "@/components/ui/button";
 import { SOURCE_CODE_GITHUB } from "@/constants";
 import { useT } from "@/hooks";
 import { GitHub as GitHubIcon } from "@mui/icons-material";
+import { Suspense } from "react";
 
 export const GithubButton = () => {
     const { t } = useT();
 
     return (
-        <Link href={SOURCE_CODE_GITHUB} isOpenInNewTab>
-            <Button
-                variant="ghost"
-                size="icon"
-                title={t("header.github-link-tooltip")}
-            >
-                <GitHubIcon />
-            </Button>
-        </Link>
+        <Suspense>
+            <Link href={SOURCE_CODE_GITHUB} isOpenInNewTab>
+                <Button
+                    variant="ghost"
+                    size="icon"
+                    title={t("header.github-link-tooltip")}
+                >
+                    <GitHubIcon />
+                </Button>
+            </Link>
+        </Suspense>
     );
 };
--- src/components/toggle-theme.tsx
+++ src/components/toggle-theme.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import React from "react";
+import React, { Suspense } from "react";
 
 import { Button } from "@/components/ui/button";
 import { useT } from "@/hooks";
@@ -15,13 +15,15 @@ export const ToggleTheme = () => {
     const newTheme = theme === "dark" ? "light" : "dark";
 
     return (
-        <Button
-            variant="ghost"
-            size="icon"
-            title={t("header.toggle-theme-tooltip", { theme: newTheme })}
-            onClick={() => setTheme(newTheme)}
-        >
-            {theme === "dark" ? <Sun /> : <Moon />}
-        </Button>
+        <Suspense>
+            <Button
+                variant="ghost"
+                size="icon"
+                title={t("header.toggle-theme-tooltip", { theme: newTheme })}
+                onClick={() => setTheme(newTheme)}
+            >
+                {theme === "dark" ? <Sun /> : <Moon />}
+            </Button>
+        </Suspense>
     );
 };
