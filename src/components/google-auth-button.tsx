6a88f788c0e2a8320ae38b255ea2a5f4d7dbb88e
suzuki3jp <suzuki3.info@gmail.com>
2025-01-09 07:56:06 +0900

    refactor: Simplify GoogleAuthButton component structure

--- src/components/google-auth-button.tsx
+++ src/components/google-auth-button.tsx
@@ -24,47 +24,45 @@ export const GoogleAuthButton = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return !data ? (
-        <>
-            <Dialog open={isOpen} onOpenChange={setIsOpen}>
-                <DialogTrigger asChild>
-                    <Button>
-                        <GoogleIcon /> {t("header.google-auth.sign-in")}
-                    </Button>
-                </DialogTrigger>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>
-                            {t("header.google-auth.agreement.title")}
-                        </DialogTitle>
-                        <DialogDescription>
-                            <Trans
-                                i18nKey="header.google-auth.agreement.content"
-                                components={{
-                                    1: (
-                                        <Link
-                                            href="/terms-and-privacy"
-                                            underline
-                                            onClick={() => setIsOpen(false)}
-                                        />
-                                    ),
-                                }}
-                            />
-                        </DialogDescription>
-                    </DialogHeader>
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <DialogTrigger asChild>
+                <Button>
+                    <GoogleIcon /> {t("header.google-auth.sign-in")}
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        {t("header.google-auth.agreement.title")}
+                    </DialogTitle>
+                    <DialogDescription>
+                        <Trans
+                            i18nKey="header.google-auth.agreement.content"
+                            components={{
+                                1: (
+                                    <Link
+                                        href="/terms-and-privacy"
+                                        underline
+                                        onClick={() => setIsOpen(false)}
+                                    />
+                                ),
+                            }}
+                        />
+                    </DialogDescription>
+                </DialogHeader>
 
-                    <DialogFooter>
-                        <DialogClose asChild>
-                            <Button variant="secondary">
-                                {t("header.google-auth.agreement.no")}
-                            </Button>
-                        </DialogClose>
-                        <Button type="submit" onClick={() => signIn("google")}>
-                            {t("header.google-auth.agreement.yes")}
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button variant="secondary">
+                            {t("header.google-auth.agreement.no")}
                         </Button>
-                    </DialogFooter>
-                </DialogContent>
-            </Dialog>
-        </>
+                    </DialogClose>
+                    <Button type="submit" onClick={() => signIn("google")}>
+                        {t("header.google-auth.agreement.yes")}
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
     ) : (
         <Button onClick={() => signOut()}>
             {t("header.google-auth.sign-out")}
