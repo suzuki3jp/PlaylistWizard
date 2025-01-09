dfe204db4b2d0af4c32cff75b84d329a9acf8a58
suzuki3jp <suzuki3.info@gmail.com>
2025-01-10 07:35:41 +0900

    feat: Add `YourPlaylists` section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import { AboutSection } from "@/components/about-section";
+import { YourPlaylists } from "@/components/your-playlists";
 import type { PageProps } from "@/types";
 
 export default async function Home(props: PageProps) {
-    return <AboutSection {...props} />;
+    return (
+        <>
+            <YourPlaylists {...props} />
+            <AboutSection {...props} />
+        </>
+    );
 }
---
+++ src/components/ui/tooltip.tsx
@@ -0,0 +1,31 @@
+"use client";
+import type React from "react";
+
+import {
+    TooltipContent,
+    Tooltip as TooltipPrimitive,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/shadcn-ui/tooltip";
+
+/**
+ * The Tooltip component.
+ * It is a wrapper around the `shadcn-ui/tooltip` component.
+ * @param param0
+ * @returns
+ */
+export const Tooltip: React.FC<TooltipProps> = ({ content, children }) => {
+    return (
+        <TooltipProvider>
+            <TooltipPrimitive>
+                <TooltipTrigger asChild>{children}</TooltipTrigger>
+                <TooltipContent>{content}</TooltipContent>
+            </TooltipPrimitive>
+        </TooltipProvider>
+    );
+};
+
+export type TooltipProps = Readonly<{
+    content: React.ReactNode;
+    children: React.ReactNode;
+}>;
