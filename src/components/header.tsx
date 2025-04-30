79cb6cf56480dc0658c5e24431f939b861edf5ce
suzuki3jp <suzuki3.info@gmail.com>
2025-04-30 10:36:28 +0900

    chore: wrap project name with home link in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,17 +11,19 @@ export function Header({ t }: HeaderProps) {
     return (
         <header className="sticky top-0 z-50 w-full border-b border-gray-800 bg-gray-950">
             <div className="container px-4 flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-                <div className="flex gap-2 items-center text-xl font-bold text-white">
-                    <div className="relative w-8 h-8">
-                        <Image
-                            src={Icon}
-                            width={32}
-                            height={32}
-                            alt="PlaylistWizard logo"
-                        />
+                <Link href="/">
+                    <div className="flex gap-2 items-center text-xl font-bold text-white">
+                        <div className="relative w-8 h-8">
+                            <Image
+                                src={Icon}
+                                width={32}
+                                height={32}
+                                alt="PlaylistWizard logo"
+                            />
+                        </div>
+                        PlaylistWizard
                     </div>
-                    PlaylistWizard
-                </div>
+                </Link>
                 <div className="flex flex-1 items-center justify-end space-x-4">
                     <nav className="flex items-center space-x-6">
                         <Link
