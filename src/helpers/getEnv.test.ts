16ef2f7cd22d6559738da88397a17024491aee85
suzuki3jp <suzuki3.info@gmail.com>
2025-05-09 13:25:56 +0900

    test: add tests for `getEnv` helper

---
+++ src/helpers/getEnv.test.ts
@@ -0,0 +1,33 @@
+import { beforeEach, describe, expect, it } from "vitest";
+
+import { err, ok } from "neverthrow";
+import { EnvError, getEnv } from "./getEnv";
+
+describe("getEnv of helpers", () => {
+    beforeEach(() => {
+        process.env.GET_ENV_TEST = "test";
+        process.env.GET_ENV_TEST_2 = "test2";
+    });
+
+    it("should return the env variable if it exists", () => {
+        const result = getEnv(["GET_ENV_TEST"]);
+        expect(result.isOk()).toBe(true);
+        expect(result).toEqual(ok(["test"]));
+    });
+
+    it("should return the env variables in correct order if multiple keys are provided", () => {
+        const result = getEnv(["GET_ENV_TEST_2", "GET_ENV_TEST"]);
+        expect(result.isOk()).toBe(true);
+        expect(result).toEqual(ok(["test2", "test"]));
+
+        const result2 = getEnv(["GET_ENV_TEST", "GET_ENV_TEST_2"]);
+        expect(result2.isOk()).toBe(true);
+        expect(result2).toEqual(ok(["test", "test2"]));
+    });
+
+    it("should return an error if any of the env variables are missing", () => {
+        const result = getEnv(["GET_ENV_TEST", "MISSING_ENV"]);
+        expect(result.isOk()).toBe(false);
+        expect(result).toEqual(err(new EnvError("MISSING_ENV")));
+    });
+});
