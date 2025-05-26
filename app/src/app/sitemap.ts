5dd419213ebf798ba9a2bb0ceb4bf7aea0c6c5dc
suzuki3jp <suzuki3.info@gmail.com>
2025-05-26 12:57:06 +0900

    feat(app): add sitemap
    Closes: #153

---
+++ app/src/app/sitemap.ts
@@ -0,0 +1,19 @@
+import type { MetadataRoute } from "next";
+
+import { supportedLangs } from "@/features/localization/i18n";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const BASE_URL = "https://playlistwizard.suzuki3.jp";
+
+  const urls = ["/", "/terms-and-privacy"];
+  const localizedUrls = supportedLangs.flatMap((lang) =>
+    urls.map((url) => `/${lang}${url}`),
+  );
+  const fullUrls = localizedUrls.map((url) => `${BASE_URL}${url}`);
+
+  // We don't set changefreq or priority here, because Google will ignore them
+  return fullUrls.map((url) => ({
+    url,
+    lastModified: new Date(),
+  }));
+}
