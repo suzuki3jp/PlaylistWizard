dfc1eb0cc7aba9c4c60250871cc35ceb7e0fd9e9
suzuki3jp <suzuki3.info@gmail.com>
2025-05-26 16:31:25 +0900

    chore(youtube): export structures

--- packages/youtube/src/index.ts
+++ packages/youtube/src/index.ts
@@ -1,5 +1,6 @@
 export { ApiClient, ApiClientOptions } from "./ApiClient";
 export { Page, PaginationOptions } from "./Page";
 export { Playlist } from "./structures/Playlist";
+export { PlaylistItem } from "./structures/PlaylistItem";
 export { Thumbnail, Thumbnails } from "./structures/Thumbnails";
 export { PlaylistManager } from "./managers/PlaylistManager";
