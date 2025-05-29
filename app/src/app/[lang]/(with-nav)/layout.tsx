0d1e0eecf2723929161c6e32e803af99606a7def
suzuki3jp <suzuki3.info@gmail.com>
2025-05-29 09:31:10 +0900

    chore(app): split Rootlayout to Rootlayout and NavigationLayout

--- app/src/app/[lang]/(with-nav)/layout.tsx
+++ app/src/app/[lang]/(with-nav)/layout.tsx
@@ -1,58 +1,22 @@
-import { GoogleAnalytics } from "@next/third-parties/google";
-import { dir } from "i18next";
-import type { Metadata } from "next";
 import type { PropsWithChildren } from "react";
-import "@/styles/globals.css";
 
 import type { SSRProps } from "@/@types";
-import { Providers } from "@/components/provider";
 import { Footer } from "@/features/footer/components/footer";
 import { Header } from "@/features/header/components/header";
 import { useServerT } from "@/features/localization/hooks/server";
-import { supportedLangs } from "@/features/localization/i18n";
-import { getEnv } from "@/helpers/getEnv";
 
-export async function generateMetadata({
-  params,
-}: SSRProps): Promise<Metadata> {
-  const { lang } = await params;
-  const { t } = await useServerT(lang);
-
-  return {
-    title: t("meta.title"),
-    description: t("meta.description"),
-  };
-}
-
-export const generateStaticParams = () => {
-  return supportedLangs.map((lang) => ({ lang }));
-};
-
-export default async function RootLayout({
+export default async function NavigationLayout({
   children,
   params,
 }: PropsWithChildren<SSRProps>) {
   const { lang } = await params;
   const { t } = await useServerT(lang);
-  const gaId = getEnv(["GOOGLE_ANALYTICS_ID"]);
-  if (gaId.isErr()) throw gaId.error;
 
   return (
-    <html lang={lang} dir={dir(lang)}>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
-
-      <GoogleAnalytics gaId={gaId.value[0]} />
-      <body className="antialiased">
-        <div className="flex min-h-screen flex-col bg-gray-950">
-          <Providers>
-            <Header t={t} lang={lang} />
-            {children}
-            <Footer t={t} lang={lang} />
-          </Providers>
-        </div>
-      </body>
-    </html>
+    <>
+      <Header t={t} lang={lang} />
+      {children}
+      <Footer t={t} lang={lang} />
+    </>
   );
 }
