1847a51bb5daad2c554781c96a2c998f8e59692d
suzuki3jp <suzuki3.info@gmail.com>
2025-05-30 16:35:44 +0900

    fix(app): prevent overwriting of default HighlightedLink style

--- app/src/presentation/common/highlighted-link.tsx
+++ app/src/presentation/common/highlighted-link.tsx
@@ -1,9 +1,7 @@
 import { cn } from "@/lib/utils";
 import { Link, type LinkProps } from "@/presentation/common/link";
 
-export function HighlightedLink(props: LinkProps) {
+export function HighlightedLink({ className, ...props }: LinkProps) {
   const defaultStyle = "text-pink-400 hover:text-pink-300";
-  return (
-    <Link className={cn(defaultStyle, props.className)} underline {...props} />
-  );
+  return <Link className={cn(defaultStyle, className)} underline {...props} />;
 }
