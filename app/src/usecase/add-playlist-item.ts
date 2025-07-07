0754da12eec281e90662b84470bf3c29b382ea92
suzuki3jp <suzuki3.info@gmail.com>
2025-07-07 09:15:25 +0900

    feat(app): implement Structured Playlists synchronization

---
+++ app/src/usecase/add-playlist-item.ts
@@ -0,0 +1,35 @@
+import { type Result, err, ok } from "neverthrow";
+
+import { callWithRetries } from "@/common/call-with-retries";
+import { PlaylistItem, type PlaylistItemInterface } from "@/entity";
+import type { ProviderRepositoryType } from "@/repository/providers/factory";
+import { addPlaylistItem } from "./actions/add-playlist-item";
+import type { Failure } from "./actions/plain-result";
+
+export class AddPlaylistItemUsecase {
+  constructor(private options: AddPlaylistItemUsecaseOptions) {}
+
+  public async execute(): Promise<Result<PlaylistItemInterface, Failure>> {
+    const { accessToken, repository, playlistId, resourceId } = this.options;
+
+    const result = await callWithRetries(
+      { func: addPlaylistItem },
+      {
+        playlistId,
+        resourceId,
+        token: accessToken,
+        repository,
+      },
+    );
+    return result.status === 200
+      ? ok(new PlaylistItem(result.data))
+      : err(result);
+  }
+}
+
+export interface AddPlaylistItemUsecaseOptions {
+  accessToken: string;
+  repository: ProviderRepositoryType;
+  playlistId: string;
+  resourceId: string;
+}
