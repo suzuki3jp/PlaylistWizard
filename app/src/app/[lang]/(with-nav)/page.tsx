6907b589c11b6a454c7f145e832a4e850b26d2fd
suzuki3jp <suzuki3.info@gmail.com>
2025-07-20 18:34:37 +0900

    refactor(app): move home page components to `presentation/pages/home`

--- app/src/app/[lang]/(with-nav)/page.tsx
+++ app/src/app/[lang]/(with-nav)/page.tsx
@@ -1,18 +1,8 @@
 import type { SSRProps } from "@/@types";
-import { Faq } from "@/presentation/home/faq";
-import { Features } from "@/presentation/home/features";
-import { Hero } from "@/presentation/home/hero";
-import { useServerT } from "@/presentation/hooks/t/server";
+import { HomePage } from "@/presentation/pages/home";
 
-export default async function Home({ params }: SSRProps) {
+export default async function ({ params }: SSRProps) {
   const { lang } = await params;
-  const { t } = await useServerT(lang);
 
-  return (
-    <main>
-      <Hero t={t} />
-      <Features t={t} />
-      <Faq t={t} />
-    </main>
-  );
+  return <HomePage lang={lang} />;
 }
