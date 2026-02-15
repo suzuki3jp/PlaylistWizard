620c8c5fe65ee00277e5fa9d36237311d5bb2e0b
suzuki3jp <suzuki3.info@gmail.com>
2026-02-15 15:30:46 +0900

    fix(app): close sidebar by default on the home page

--- app/src/presentation/pages/layouts/sidebar-layout.tsx
+++ app/src/presentation/pages/layouts/sidebar-layout.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import type { PropsWithChildren } from "react";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { DEFAULT_CLOSE_SIDEBAR_ROUTES } from "@/constants";
 import { AppSidebar } from "@/features/side-menu";
 
 export function SidebarLayout({ children }: PropsWithChildren) {
+  const pathname = usePathname();
+
   return (
     <SidebarProvider
-      defaultOpen={true}
+      defaultOpen={!DEFAULT_CLOSE_SIDEBAR_ROUTES.includes(pathname)}
       style={{ "--sidebar-width": "300px" } as React.CSSProperties}
     >
       <AppSidebar className="top-16 z-30 h-[calc(100svh-4rem)]" />
