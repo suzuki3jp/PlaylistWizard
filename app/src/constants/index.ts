0020ed58679f0438d600db4c6d7a4b47e92fd735
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2026-03-04 12:28:42 +0900

    feat(app): add searchParams constants for URL query keys

--- app/src/constants/index.ts
+++ app/src/constants/index.ts
@@ -1,5 +1,6 @@
 export * as ga4Events from "./ga4-events";
 export * as queryKeys from "./query-keys";
+export * as searchParams from "./search-params";
 export * as urls from "./urls";
 
 export const VERSION = `v${process.env.RELEASE}`;
