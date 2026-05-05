86d88b769b93efd9d8472554166ed6c1f63212f9
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2026-05-05 17:58:15 +0900

    fix(app): use default web database pool size

--- apps/www/src/lib/db/index.ts
+++ apps/www/src/lib/db/index.ts
@@ -3,7 +3,6 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 const client = postgres(process.env.DATABASE_URL!, {
-  max: 1,
   prepare: false,
 });
 export const db = drizzle(client, { schema });
